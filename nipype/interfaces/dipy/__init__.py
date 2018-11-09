ca26b2e2912ad077c0c3f78f321f27771d957572
Martin Grignard <mar.grignard@gmail.com>
2018-11-09 14:06:14 +0100

    Add EstimateConductivity
    Add EstimateConductivity to Dipy interface.

--- nipype/interfaces/dipy/__init__.py
+++ nipype/interfaces/dipy/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .tracks import StreamlineTractography, TrackDensityMap
-from .tensors import TensorMode, DTI
+from .tensors import TensorMode, DTI, EstimateConductivity
 from .preprocess import Resample, Denoise
 from .reconstruction import RESTORE, EstimateResponseSH, CSD
 from .simulate import SimulateMultiTensor
